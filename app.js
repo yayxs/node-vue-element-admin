0f59ac8a2a8ae0e290d011bf45da2b28122a8df9
yayxs <leaderywl@163.com>
2020-11-09 22:01:37 +0800

    添加静态资源服务

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-var createError = require("http-errors");
+const createError = require("http-errors");
 const express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
 const app = express();
 
-// view engine setup
+app.use("/users", require("./routes/users"));
+app.use("/upload", require("./routes/upload"));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-// app.use(express.static("public"));
 app.use(logger("dev"));
 // body参数解析
 app.use(express.json());
@@ -23,29 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-/**
- * desc 路径匹配中间件
- * path
- * cb
- */
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// 静态资源部署 静态资源服务器
+app.use(express.static("./build"));
+
+app.use((req, res, next) => {
   next(createError(404));
-});
-
-// 注册普通的中间件
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
 });
 
 module.exports = app;
